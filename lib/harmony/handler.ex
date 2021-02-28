c10d41a0aa5a1ac739174a2a04e100ec12d694f0
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-03-01 01:53:38 +0700

    Added the module documentations.

--- lib/harmony/handler.ex
+++ lib/harmony/handler.ex
@@ -1,6 +1,15 @@
 defmodule Harmony.Handler do
+
+  @moduledoc """
+  Handler for HTTP requests.
+  """
+
+
   require Logger
 
+  @doc """
+  Transforms HTTP request into response
+  """
   def handle(request) do
     request
     |> parse
@@ -11,33 +20,60 @@ defmodule Harmony.Handler do
     |> format_response
   end
 
+  @doc """
+  Tracks HTTP requests that returns 404 for debugging purposes.
+  """
   def track(%{status: 404, path: path} = conversation) do
     Logger.warn "Warning: User trying to access #{path}, where page not exists for such path."
     conversation
   end
 
+  @doc """
+  Captures any other request than 404, when calling track function.
+  """
   def track(conversation), do: conversation
 
+  @doc """
+  Rewrite the path of the requests /home to /servers
+  """
   def rewrite_path(%{path: "/home"} = conversation) do
     %{conversation | path: "/servers"}
   end
 
+  @doc """
+  Rewrite the path with the special syntaxes of '?=' into a standard url
+  """
   def rewrite_path(%{path: path} = conv) do
     regex = ~r{\/(?<slug>\w+)\?id=(?<id>\d+)}
     captures = Regex.named_captures(regex, path)
     rewrite_path_captures(conv, captures)
   end
 
+  @doc """
+  Capture any other path that doesnt contain '?='
+  """
   def rewrite_path(conversation), do: conversation
 
-  def rewrite_path_captures(conversation, %{"slug" => slug, "id" => id}) do
+  @doc """
+  Updates the conversation's path according to the slug given by the rewrite_path function.
+  """
+  defp rewrite_path_captures(conversation, %{"slug" => slug, "id" => id}) do
     %{conversation | path: "/#{slug}/#{id}"}
   end
 
-  def rewrite_path_captures(conversation, nil), do: conversation
+  @doc """
+  Captures any other conversation that doesn't contain '?='
+  """
+  defp rewrite_path_captures(conversation, nil), do: conversation
 
+  @doc """
+  Logs the parsed request for debugging purposes.
+  """
   def log(conv), do: IO.inspect conv
 
+  @doc """
+  Parses the request into a map that can be used for routing.
+  """
   def parse(request) do
     [method, path, _http_version] =
       request
@@ -47,6 +83,9 @@ defmodule Harmony.Handler do
     %{method: method, path: path, response_body: "", status: nil}
   end
 
+  @doc """
+  Handling the route access to /server/new to open the html page register.html
+  """
   def route(%{method: "GET", path: "/servers/new", response_body: ""} = conversation) do
     Path.expand("../../web/pages", __DIR__)
     |> Path.join("register.html")
@@ -54,6 +93,9 @@ defmodule Harmony.Handler do
     |> handle_file(conversation)
   end
 
+  @doc """
+  Handling the route access to info/<page-name> to open the appropriate <page-name>.html page
+  """
   def route(%{method: "GET", path: "/info/" <> page, response_body: ""} = conversation) do
     Path.expand("../../web/pages", __DIR__)
     |> Path.join(page <> ".html")
@@ -61,10 +103,16 @@ defmodule Harmony.Handler do
     |> handle_file(conversation)
   end
 
+  @doc """
+  Handles the route access to /servers
+  """
   def route(%{method: "GET", path: "/servers", response_body: ""} = conversation) do
     %{conversation | status: 200, response_body: "Available Servers:\nLearnFlutter, LearnElixir, LearnPhoenixFramework"}
   end
 
+  @doc """
+  Handles the route access to specific Harmony servers denoted by their id that is defined as string.
+  """
   def route(%{method: "GET", path: "/servers/" <> id, response_body: ""} = conversation) do
     case id do
       "1" -> %{conversation | status: 200, response_body: "Welcome to Learn Flutter Server!"}
@@ -74,29 +122,45 @@ defmodule Harmony.Handler do
     end
   end
 
+  @doc """
+  Handles the route access to /servers/id with the delete method.
+  """
   def route(%{method: "DELETE", path: "/servers/" <> id, response_body: ""} = conversation) do
     %{conversation | status: 403, response_body: "Insufficient user privileges to delete the server."}
   end
 
+  @doc """
+  Handles accessing to non existing page in the server.
+  """
   def route(%{path: path} = conversation) do
     %{conversation | status: 404, response_body: "Page not found."}
   end
 
+  @doc """
+  Update the map to contain the content of the requested page file.
+  """
   def handle_file({:ok, file_content}, conversation) do
     %{conversation | status: 200, response_body: file_content}
   end
 
+  @doc """
+  Update the map for the case that the page file is not found in the server.
+  """
   def handle_file({:error, :enoent}, conversation) do
     %{conversation | status: 404, response_body: "Page not found."}
   end
 
+  @doc """
+  Update the map for other cases where there is problems when opening the page.
+  """
   def handle_file({:error, reason}, conversation) do
     %{conversation | status: 500, response_body: "Error on reading file. (#{reason})"}
   end
 
+  @doc """
+  Format the response according to the parsed map.
+  """
   def format_response(conversation) do
-    # Transformation:
-    # Transform it into the following:
     expected_response = """
     HTTP/1.1 #{conversation.status} #{status_reason(conversation.status)}
     Content-Type: text/html
@@ -106,12 +170,21 @@ defmodule Harmony.Handler do
     """
   end
 
+  @doc """
+  Function to add emojis to the response body of a request that has status code of 200.
+  """
   defp emojify(%{status: 200, response_body: body} = conversation) do
     %{conversation | response_body: "👍" <> body <> "👍"}
   end
 
+  @doc """
+  Function to not add emojis to the response body for requests with status other than 200.
+  """
   defp emojify(conversation), do: conversation
 
+  @doc """
+  Returns the string representation of the given status code.
+  """
   defp status_reason(code) do
     %{
       200 => "OK",
