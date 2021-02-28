c8390f7f1e0a512107efe568481b6f56a2b3ca6e
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-02-28 21:11:33 +0700

    Added HTTP status, and routing for specific servers.

--- lib/harmony/handler.ex
+++ lib/harmony/handler.ex
@@ -2,9 +2,12 @@ defmodule Harmony.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
   end
+
+  def log(conv), do: IO.inspect conv
 
   def parse(request) do
     # Input Request:
@@ -23,11 +26,21 @@ defmodule Harmony.Handler do
       |> Enum.at(0) # alternatively List.first(list)
       |> String.split(" ")
     # Output:
-    %{method: method, path: path, response_body: ""}
+    %{method: method, path: path, response_body: "", status: nil}
   end
 
   def route(%{method: "GET", path: "/servers", response_body: ""} = conversation) do
-    %{conversation | response_body: "LearnFlutter, LearnElixir, LearnPhoenixFramework"}
+    %{conversation | status: 200, response_body: "LearnFlutter, LearnElixir, LearnPhoenixFramework"}
+  end
+
+  def route(%{method: "GET", path: "/servers/" <> id, response_body: ""} = conversation) do
+    case id do
+      "1" -> %{conversation | status: 200, response_body: "Welcome to Learn Flutter Server!"}
+      "2" -> %{conversation | status: 200, response_body: "Welcome to Learn Elixir Server!"}
+      "3" -> %{conversation | status: 200, response_body: "Welcome to Learn Phoenix Framework Server!"}
+      _ -> %{conversation | status: 404, response_body: "Server not found!"}
+    end
+
   end
 
   def route(%{method: "GET", response_body: ""} = conversation) do
@@ -38,12 +51,23 @@ defmodule Harmony.Handler do
     # Transformation:
     # Transform it into the following:
     expected_response = """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conversation.status} #{status_reason(conversation.status)}
     Content-Type: text/html
     Content-Length: #{byte_size(conversation.response_body)}
 
     #{conversation.response_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
 
