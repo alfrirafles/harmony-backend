7e9bcc91ef121c2c1a1d772466e91a5fc9abd438
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-03-01 21:39:36 +0700

    Added documentation for conversation module.

--- lib/harmony/conversation.ex
+++ lib/harmony/conversation.ex
@@ -1,9 +1,16 @@
 defmodule Harmony.Conversation do
 
+  @moduledoc """
+  Module that provides type safety for request -> response conversation in the server.
+  """
+
   alias Harmony.Conversation
 
   defstruct [method: "", path: "", status: nil, response_body: ""]
 
+  @doc """
+  Return the string required by the response for a request that explains about the status.
+  """
   def full_status(conversation) do
     "#{conversation.status} #{status_reason(conversation.status)}"
   end
