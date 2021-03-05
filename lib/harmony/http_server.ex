a933c20728eb806411be827a279deaed8957bfec
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-03-05 22:40:24 +0700

    Refactored server, added `accept_loop` function to loop the socket to accept connections.

--- lib/harmony/http_server.ex
+++ lib/harmony/http_server.ex
@@ -8,8 +8,18 @@ defmodule Harmony.HttpServer do
 #  end
 
   def start(port) when is_integer(port) and port > 1023 do
-    IO.puts "Listening for connection request on port #{port}..."
+    IO.puts "🎧 Listening for connection request on port #{port}..."
     {:ok, listen_socket} = :gen_tcp.listen(port, [:binary, packet: :raw, active: false, reuseaddr: true])
+    accept_loop(listen_socket)
+  end
+
+  def accept_loop(listen_socket) do
+    IO.puts "⌛️ Waiting to accept connection...\n"
+    
+    {:ok, client_socket} = :gen_tcp.accept(l_socket)
+    
+    serve(client_socket)
+    
     accept_loop(listen_socket)
   end
 end
