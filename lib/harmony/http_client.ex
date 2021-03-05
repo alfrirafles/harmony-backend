715e09a97493029cad630d41a51dd3488296a9a8
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-03-06 00:24:12 +0700

    transcribed the client function from erlang to elixir.

--- lib/harmony/http_client.ex
+++ lib/harmony/http_client.ex
@@ -1,6 +1,8 @@
-client() ->
-SomeHostInNet = "localhost", % to make it runnable on one machine
-{ok, Sock} = gen_tcp:connect(SomeHostInNet, 5678,
-[binary, {packet, 0}]),
-ok = gen_tcp:send(Sock, "Some Data"),
-ok = gen_tcp:close(Sock).
+defmodule Harmony.HttpClient do
+  def client do
+    some_host_in_net = 'localhost'
+    {:ok, socket} = :gen_tcp.connect(some_host_in_net, 4000, [:binary, packet: 0])
+    :ok = :gen_tcp.send(socket, "some data")
+    :ok = :gen_tcp.close(socket)
+  end
+end
