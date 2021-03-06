f123037fc852b7eae70e415f56c103ab691c4ac8
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-03-07 01:10:31 +0700

    Fixed bug for parsing POST request

--- lib/harmony/parser.ex
+++ lib/harmony/parser.ex
@@ -11,41 +11,57 @@ defmodule Harmony.Parser do
   """
   def parse(request) do
     [request_method | request_headers] = request
-                                   |> String.split("\r\n", trim: true)
+                                         |> String.split("\r\n", trim: true)
     [method, path, _http_version] =
       request_method
       |> String.split(" ", trim: true)
     headers = request_headers
-              |> parse_headers
-    %Conversation{
+              |> parse_headers(method)
+    conversation = %Conversation{
       method: method,
       path: path,
       request_headers: headers,
-      request_params: List.to_string(request_headers)
-                      |> parse_params(headers["Content-Type"]),
       response_body: "",
       status: nil
     }
+    if method == "POST" do
+      params = request_headers
+               |> Enum.take(-1)
+               |> List.first
+               |> parse_params(headers["Content-Type"])
+      %{conversation | request_params: params}
+    else
+      conversation
+    end
   end
 
-  defp parse_headers(header_list) do
+  def parse_headers(header_list, "GET") do
     Enum.reduce(header_list, %{}, fn header, acc ->
         [key, value] = String.split(header, ": ")
         Map.put(acc, key, value)
-      end
-    )
+      end)
   end
 
-  defp parse_params(request_body, "application/x-www-form-urlencoded") when request_body != "" do
+  def parse_headers(header_list, "POST") do
+    header_list
+    |> Enum.take(length(header_list) - 1)
+    |> parse_headers("GET")
+  end
+
+  def parse_headers(header_list, "DELETE") do
+    parse_headers(header_list, "GET")
+  end
+
+  def parse_params(request_body, "application/x-www-form-urlencoded") when request_body != "" do
     request_body
     |> String.slice(0, String.length(request_body) - 1)
     |> URI.decode_query
   end
 
-  defp parse_params(request_body, "application/json") when request_body != "" do
+  def parse_params(request_body, "application/json") when request_body != "" do
     request_body
     |> Poison.Parser.parse!(%{})
   end
 
-  defp parse_params(_request_body, nil), do: %{}
+  def parse_params(_request_body, nil), do: %{}
 end
