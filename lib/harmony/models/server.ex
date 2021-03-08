331db10f70bfc74ac1fe03a4806150f98333c982
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-03-09 06:21:26 +0700

    Added get_messages function

--- lib/harmony/models/server.ex
+++ lib/harmony/models/server.ex
@@ -1,6 +1,10 @@
 defmodule Harmony.Server do
   defstruct [id: nil, name: "", description: "", region: "", join_link: ""]
 
+  import Harmony.FileHandler, only: [read_csv: 1]
+
+  @messages_source_path Path.expand("db/data", File.cwd!)
+
   def asia_region(server) do
     server.region == "asia"
   end
@@ -11,5 +15,12 @@ defmodule Harmony.Server do
 
   def order_by_id_asc(server1, server2) do
     server1.id < server2.id
+  end
+
+  def get_messages(server_id) do
+    @messages_source_path
+    |> Path.join("messages.csv")
+    |> read_csv
+    |> Enum.filter(&(&1.server_id == server_id))
   end
 end
